f289d71869217d61b2c2462409f9403e874b6452
mikhailouski_n <jabber_mcn@tut.by>
2023-06-08 14:33:52 +0300

    initial commit 2

--- migrations/env.py
+++ migrations/env.py
@@ -1,17 +1,16 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
 import os
 import sys
 
-from src.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
-
-from src.auth.models import metadata as metadata_auth
-from src.operations.models import metadata as metadata_operations
+from src.auth.models import *
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from src.database import metadata
+from src.operations.models import *
 
 sys.path.append(os.path.join(sys.path[0], '1'))
 
@@ -35,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = metadata
 
 
 # other values from the config, defined by the needs of env.py,
